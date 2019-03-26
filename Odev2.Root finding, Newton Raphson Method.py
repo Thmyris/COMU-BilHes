9cf7c2654ac195e7224c95f857045fcb6c254f6a
Thmyris <thmyris@windowslive.com>
2019-03-27 00:14:03 +0300

    bu kod sonra duzeltilecek

--- Odev2.Root finding, Newton Raphson Method.py
+++ Odev2.Root finding, Newton Raphson Method.py
@@ -1,8 +1,9 @@
-from sympy import Symbol, Derivative
-
-
 def f(x):
     return(x**2-4*x+3)
+
+
+def dfdx(x):
+    return (2*x-4)
 
 
 x = int(input("koke yakin bir x sayisi tahmin edin: "))
@@ -12,10 +13,9 @@ if (f(x) == 0):  # yani fonksiyona gonderdigimiz x'in
     print("tahmininiz denklemin kokudur")
 else:
     print("En yakin kok hesaplaniyor...")
-    temp = x1
     for i in range(1000):
-        xr = (f(x1)*x2-f(x2)*x1)/(f(x1)-f(x2))
-        if(abs(f(temp)-f(xr)) < 0.01): #gecen kodda temp-xr demistim, y ile yaklasmak vs x ile yaklasmak arasinda hangisi daha etkili bilmiyorum.
+        tegetde
+        if(abs(f(temp)-f(xr)) < 0.01):
             print("kok bulundu: ", xr)
             print("Hesaplama bitene kadar 2ye bolme miktari: ", i+1)
             break
@@ -29,10 +29,3 @@ else:
             x1 = xr
         temp = xr
         print(xr)
-
-
-def computeDerivative():
-    x = Symbol('x')
-    function = f(x)
-    deriv = Derivative(function, x)
-    deriv.doit()
